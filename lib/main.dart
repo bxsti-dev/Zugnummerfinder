bcef624ac812baea66fb703915bcddfdd2fdf7a7
Basti <bastihd2006@gmail.com>
2026-05-05 22:42:07 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow, brightness: Brightness.dark),
+        ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  @override
+  State<Home> createState() => _HomeState();
+}
+class _HomeState extends State<Home> {
+  //Variables, functions
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          children: [
+
+            Align(
+              alignment: AlignmentGeometry.topCenter,
+              child: Padding(padding: const EdgeInsets.all(24.0),
+                child: SearchBar(
+                  hintText: "Search for trainnumber...",
+                  //leading: Icon(Icons.manage_search),
+                  padding: WidgetStatePropertyAll(EdgeInsetsGeometry.only(left: 12, right: 12)),
+                  trailing: [IconButton.filledTonal(onPressed: (){}, icon: Icon(Icons.search))],
+                ),
+              )
+            ),
+
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 8,
+                  children: [
+                    Icon(
+                      Icons.train,
+                      size: 100,
+                      color: ColorScheme.of(context).primary,
+                    ),
+                    Text("No train searched", style: TextStyle(fontSize: 20))
+                  ],
+                )
+              ),
+            )
+
+          ]
+        )
+      )
+    );
+  }
+}
