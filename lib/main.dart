8aef910c64caf165968926a13cf40e8eaae16a54
Basti <bastihd2006@gmail.com>
2026-05-19 20:15:00 +0200

    added animation

--- lib/main.dart
+++ lib/main.dart
@@ -142,54 +142,62 @@ class _HomeState extends State<Home> {
               )
             ),
 
-             
-            /// RESULT WIDGET ///
-            hasResult ? Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50, top: 20, bottom: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 8,
-                children: [
-                  Text("$name",                     style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
-                  Text("($minNumber - $maxNumber)", style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
-                  Padding(padding: EdgeInsetsGeometry.all(20)),
 
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-                    Text("Year built:", style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text("$yearBuilt")
-                  ]),
-              
-                  Divider(),
-              
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-                    Text("Retired:", style: TextStyle(fontWeight: FontWeight.bold)),
-                    retired == null ? Text("No") : Text("$retired")
-                  ]),
 
-                  type != null ? Divider():Container(),
-              
-                  type != null ? Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-                    Text("Type:", style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text("$type")
-                  ]):Container(),
-                ]
-              ),
-            )
-            
-            
-            /// NOT SEARCHED PLACEHOLDER ///
-            : Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 8,
-                  children: [
-                    Icon(Icons.train, size: 100, color: ColorScheme.of(context).primary),
-                    Text("Search for trains", style: TextStyle(fontSize: 20))
-                  ],
+            Expanded(
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 250),
+                switchInCurve: Curves.easeInBack,
+                switchOutCurve: Curves.easeOutBack,
+                /// RESULT WIDGET ///
+                child: hasResult ? Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50, top: 20, bottom: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      spacing: 8,
+                      children: [
+                        Text("$name",                     style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+                        Text("($minNumber - $maxNumber)", style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+                        Padding(padding: EdgeInsetsGeometry.all(20)),
+                              
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                          Text("Year built:", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text("$yearBuilt")
+                        ]),
+                    
+                        Divider(),
+                    
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                          Text("Retired:", style: TextStyle(fontWeight: FontWeight.bold)),
+                          retired == null ? Text("No") : Text("$retired")
+                        ]),
+                              
+                        type != null ? Divider():Container(),
+                    
+                        type != null ? Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                          Text("Type:", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text("$type")
+                        ]):Container(),
+                      ]
+                    ),
                 )
+                
+
+                
+                /// NOT SEARCHED PLACEHOLDER ///
+                : Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 8,
+                      children: [
+                        Icon(Icons.train, size: 100, color: ColorScheme.of(context).primary),
+                        Text("Search for trains", style: TextStyle(fontSize: 20))
+                      ],
+                    )
+                ),
+                
               ),
-            )
+            ),
 
           ]
         )
