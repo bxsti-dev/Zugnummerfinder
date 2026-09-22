9bfffb8643d13d65ac9d6b1775dcb8c28680f501
Basti <bastihd2006@gmail.com>
2026-09-22 18:29:30 +0200

    added max-width for web (desktop)

--- lib/main.dart
+++ lib/main.dart
@@ -154,33 +154,36 @@ class _HomeState extends State<Home> {
                   /// RESULT WIDGET ///
                   child: hasResult ? Padding(
                       padding: const EdgeInsets.only(left: 50, right: 50, top: 20, bottom: 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        spacing: 8,
-                        children: [
-                          Text("$name",                     style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
-                          Text("($minNumber - $maxNumber)", style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
-                          Padding(padding: EdgeInsetsGeometry.all(20)),
-                                
-                          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-                            Text("Year built:", style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text("$yearBuilt")
-                          ]),
-                      
-                          Divider(),
-                      
-                          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-                            Text("Retired:", style: TextStyle(fontWeight: FontWeight.bold)),
-                            retired == null ? Text("No") : Text("$retired")
-                          ]),
-                                
-                          type != null ? Divider():Container(),
-                      
-                          type != null ? Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-                            Text("Type:", style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text("$type")
-                          ]):Container(),
-                        ]
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(maxWidth: 800.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          spacing: 8,
+                          children: [
+                            Text("$name",                     style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+                            Text("($minNumber - $maxNumber)", style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+                            Padding(padding: EdgeInsetsGeometry.all(20)),
+                                  
+                            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                              Text("Year built:", style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text("$yearBuilt")
+                            ]),
+                        
+                            Divider(),
+                        
+                            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                              Text("Retired:", style: TextStyle(fontWeight: FontWeight.bold)),
+                              retired == null ? Text("No") : Text("$retired")
+                            ]),
+                                  
+                            type != null ? Divider():Container(),
+                        
+                            type != null ? Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                              Text("Type:", style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text("$type")
+                            ]):Container(),
+                          ]
+                        ),
                       ),
                   )
                   
